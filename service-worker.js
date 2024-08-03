f513531f36550a5b0ebf7647b823f15cc3fbbdb0
shiranyu411 <1580276747@qq.com>
2024-08-04 04:58:33 +0800

    Added PWA icon and updated manifest

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,27 @@
-self.addEventListener('install', event => {
+const cacheName = 'feel-temperature-calculator-v1';
+const assetsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icon.png'
+];
+
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('static-cache').then(cache => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './style.css',
-                './script.js',
-                './manifest.json',
-                './icon-192x192.png',
-                './icon-512x512.png'
-            ]);
-        })
+        caches.open(cacheName)
+            .then((cache) => {
+                return cache.addAll(assetsToCache);
+            })
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
     );
 });
